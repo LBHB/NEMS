cd73f107a00af928ea0d5fa91251f6b313743dff
jacobpennington <jacob.pennington@wsu.edu>
2022-09-07 17:01:44 -0700

    Temporarily disabled noise_corrected_r, doesn't work.

--- nems/metrics/correlation.py
+++ nems/metrics/correlation.py
@@ -26,7 +26,7 @@ def correlation(x, y):
 
 # TODO: fill in examples for single trial split
 def noise_corrected_r(prediction, single_trial_responses, n_pairs=None,
-                      trial_axis=1, channel_axis=None):
+                      trial_axis=0, channel_axis=None):
     """Compute noise-corrected correlation coefficient for a model prediction.
 
     Noise is determined by measuring single-trial correlations in the recorded
@@ -44,38 +44,43 @@ def noise_corrected_r(prediction, single_trial_responses, n_pairs=None,
         Number of random single trial pairs to test. If None, test all pairs.
     trial_axis : int; default=0.
         Axis to concatenate for arrays in `single_trial_responses`.
-        For example, if `trial_axis=1` and
-        `single_trial_responses = [array(shape=(50, 1)), array(shape=(50, 1))]`
-        then `all_trials = array(50, 2)`.
-    channel_axis : int or None; optional
+        For example, if `trial_axis=0` and
+        `single_trial_responses = [array(shape=(1, 50)), array(shape=(1, 50))]`
+        then `all_trials = array(2, 50)`.
+    channel_axis : int; optional
         Axis for multiple responses. For example,
-        if `single_trial_responses = array(shape=(1000, 10, 50))` representing
+        if `single_trial_responses = array(shape=(10, 1000, 50))` representing
         responses of 50 neurons to 10 trials of 1000 time units each, then use
         `channel_axis = 2` to compute this metric for each of the 50 neurons.
-        Otherwise if `channel_axis is None`, assume array is 2-dimensional
-        (i.e. only trial x time or time x trial)
-
+        Otherwise if `channel_axis is None`, assumes channels are represented on
+        the last axis.
     Returns
     -------
     corrected_rs : list of float
         One value per channel.
         
     """
+    # TODO
+    raise NotImplementedError(
+        "There are still some bugs to work out for this implementation."
+    )
+
     trials = np.concatenate(single_trial_responses, axis=trial_axis)
 
     if channel_axis is not None:
         n_channels = trials.shape[channel_axis]
     else:
         n_channels = 1
-        trials = np.expand_dims(trials, axis=-1)
         channel_axis = -1
     
     corrected_rs = []
     for i in range(n_channels):
-        single_channel = np.take(trials, indices=[i], axis=channel_axis)
-        trial_pair_r = _paired_single_trial_r(trials, n_pairs=n_pairs,
-                                            trial_axis=trial_axis)
-        trial_pred_r = _single_trial_r(trials, trial_axis=trial_axis)
+        one_channel_pred = np.take(prediction, indices=[i], axis=channel_axis)
+        one_channel_resp = np.take(trials, indices=[i], axis=channel_axis)
+        trial_pair_r = _paired_single_trial_r(one_channel_resp, n_pairs=n_pairs,
+                                              trial_axis=trial_axis)
+        trial_pred_r = _single_trial_r(one_channel_pred, one_channel_resp,
+                                       trial_axis=trial_axis)
         corrected_r= np.mean(trial_pred_r)/np.sqrt(trial_pair_r)
         corrected_rs.append(corrected_r)
     
@@ -105,10 +110,12 @@ def _paired_single_trial_r(trials, n_pairs=1000, trial_axis=0, limit=0.01):
     n_repetitions = trials.shape[trial_axis]
     # Get all pairs of indices into trial axis,
     # then randomly shuffle the pairs
-    pairs = itertools.permutations(range(n_repetitions), 2)[:n_pairs]
+    pairs = itertools.permutations(range(n_repetitions), 2)
     if pairs is None:
         # Only 1 repetition
         return 0  # TODO: why 0?
+    else:
+        pairs = list(pairs)[:n_pairs]
     np.random.shuffle(pairs)
 
     # Compute correlation of each pair of responses,
