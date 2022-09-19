a38464efd25fd911ce8d8d61c168a2f385206c1f
jacobpennington <jacob.pennington@wsu.edu>
2022-09-19 08:42:07 -0700

    Removed old comments, started simplifying and optimizing

--- nems/layers/stp.py
+++ nems/layers/stp.py
@@ -1,15 +1,9 @@
 import numpy as np
+import numexpr as ne
 
 from nems.registry import layer
 from nems.layers.base import Layer, Phi, Parameter, require_shape
 from nems.distributions import Normal, HalfNormal
-
-
-def _cumtrapz(x, dx=1., initial=0., axis=1):
-    x = (x[:, :-1] + x[:, 1:]) / 2.0
-    x = np.pad(x, ((0, 0), (1, 0)), 'constant', constant_values=(initial, initial))
-    # x = tf.pad(x, ((0, 0), (1, 0), (0, 0)), constant_values=initial)
-    return np.cumsum(x, axis=axis) * dx
 
 
 class ShortTermPlasticity(Layer):
@@ -17,8 +11,38 @@ class ShortTermPlasticity(Layer):
     def __init__(self, fs=100, quick_eval=True, crosstalk=0, x0=None,
                  dep_only=True, chunksize=5, reset_signal=None,
                  **kwargs):
-        require_shape(self, kwargs, minimum_ndim=1)
+        """TODO: docs
 
+        TODO: additional context.
+        Need @SVD's help filling in all these docs, some parameter renaming
+        would likely be helpful as well.
+        
+        Parameters
+        ----------
+        shape : tuple.
+            TODO: what's the shape syntax? Only one dim, multiple? What do
+                  the value(s) represent?
+        fs : int; default=100.
+            Sampling rate. TODO: explain what this is used for.
+        quick_eval : bool; default=True.
+            Determines which implementation of the STP algorithm to use.
+            TODO: explain why some one might want to use `quick_eval=False`
+            (or if there's no good reason, just get rid of it). I think for now
+            it's just b/c faciliatation isn't implemented?
+        crosstalk : int; default=0;
+            TODO: explain what this does.
+        x0 : TODO: type?; optional.
+            TODO: explain what this does.
+        dep_only : bool; default=True.
+            TODO: explain what this does.
+        chunksize : int; default=5.
+            TODO: explain what this does.
+        reset_signal : TODO: type?; optional.
+            TODO: explain what this does.
+        
+        """
+
+        require_shape(self, kwargs, minimum_ndim=1)
         self.fs = fs
         self.quick_eval = quick_eval
         self.crosstalk = crosstalk
@@ -26,46 +50,85 @@ class ShortTermPlasticity(Layer):
         self.dep_only = dep_only
         self.chunksize = chunksize
         self.reset_signal = reset_signal
-
+        self._reset_times = None
         super().__init__(**kwargs)
 
+
     def initial_parameters(self):
+        """TODO: docs
+        
+        Layer parameters
+        ----------------
+        u : np.ndarray.
+            TODO: explain purpose.
+            TODO: expected shape?
+            Prior: HalfNormal(sd=0.1)
+            Bounds: (0.0001, np.inf)   TODO: should this use epsilon instead?
+        tau : np.ndarray.
+            TODO: explain purpose.
+            TODO: expected shape?
+            Prior: HalfNormal(sd=0.1)
+            Bounds: (1e-6, np.inf)     TODO: should this use epsilon instead?
+        
+        """
+        tau_sd = np.full(shape=self.shape, fill_value=0.1)
+        tau_prior = HalfNormal(tau_sd)
+        u_sd = np.full(shape=self.shape, fill_value=0.1)
+        u_prior = HalfNormal(u_sd)
 
-        taumean = np.full(shape=self.shape, fill_value=0.1)
-        tauprior = HalfNormal(taumean)
-        umean = np.full(shape=self.shape, fill_value=0.1)
-        uprior = HalfNormal(umean)
-
-        tau_bounds = (0.0001, np.inf)
+        # TODO: is setting this based on sampling rate really necessary?
+        #       if so, explain why
+        tau_min = max(0.0001, self.fs/2)
+        tau_bounds = (tau_min, np.inf)
         u_bounds = (1e-6, np.inf)
 
-        u = Parameter('u', shape=self.shape, prior=uprior, bounds=u_bounds)
-        tau = Parameter('tau', shape=self.shape, prior=tauprior, bounds=tau_bounds)
+        u = Parameter('u', shape=self.shape, prior=u_prior, bounds=u_bounds)
+        tau = Parameter('tau', shape=self.shape, prior=tau_prior,
+                        bounds=tau_bounds)
 
         return Phi(u, tau)
 
-
+    # Authors: SVD, Menoua K.
+    # Revision by: JRP.
     def evaluate(self, input):
-        """
-        STP core function
-        ported from nems0.modules.stp
+        """TODO: docs
 
-        def _stp(X, u, tau, x0=None, crosstalk=0, fs=1, reset_signal=None, quick_eval=False, dep_only=False,
-                 chunksize=5):
+        TODO: is there a succint equation we can put here to represent what
+              STP is doing?
+
+        TODO: brief explanation of what the Layer is doing to inputs.
+
         """
 
         u, tau = self.get_parameter_values()
 
         # TODO: get rid of need for transpose here
-        tstim = input.astype('float64').T
+        tstim = input.T
         s = tstim.shape
-        u = u.astype('float64')
-        tau = tau.astype('float64')
+
+        # Input shape should not change (otherwise other parameters would
+        # break anyway), so this only needs to be calculated on the first
+        # evaluation. Saves almost a millisecond per eval (formerly per loop).
+        # TODO: profile memory usage on this. Only saves 10 microseconds
+        #       (for the arange version), so if it's a huge memory increase then
+        #       this may not be worth caching.
+        if self._reset_times is None:
+            if self.reset_signal is None:
+                reset_times = np.arange(0, s[1] + chunksize - 1, chunksize)
+            else:
+                reset_times = np.argwhere(self.reset_signal[0, :])[:, 0]
+                reset_times = np.append(reset_times, s[1])
+            self._reset_times = reset_times
+        else:
+            reset_times = self._reset_times
 
         tstim[np.isnan(tstim)] = 0
         if self.x0 is not None:
             tstim -= np.expand_dims(self.x0, axis=1)
 
+        # TODO: Is this necessary? If it's just to remove negative values, would
+        #       shifting (and then unshifting at the end) be sufficient? 
+        #       Clipping values like this tends to make optimization harder.
         tstim[tstim < 0] = 0
 
         # TODO: deal with upper and lower bounds on dep and facilitation parms
@@ -75,30 +138,16 @@ class ShortTermPlasticity(Layer):
         # limits, assumes input (X) range is approximately -1 to +1
         # TODO: add support for facilitation???
         if self.dep_only or self.quick_eval:
-            ui = np.abs(u.copy())
-        else:
-            ui = u.copy()
+            u = np.abs(u)
 
-        # force tau to have positive sign (probably better done with bounds on fitter)
-        taui = np.absolute(tau.copy())
-        taui[taui < 2 / self.fs] = 2 / self.fs
-
-        # ui[ui > 1] = 1
-        # ui[ui < -0.4] = -0.4
-
-        # avoid ringing if combination of strong depression and
-        # rapid recovery is too large
-        # rat = ui**2 / taui
-
-        # MK comment out
-        # ui[rat>0.1] = np.sqrt(0.1 * taui[rat>0.1])
-
-        # taui[rat>0.08] = (ui[rat>0.08]**2) / 0.08
-        # print("rat: %s" % (ui**2 / taui))
-
+        # TODO: specify parameters in bin units in the first place so that we
+        #       don't need to bother with this? Can still add a separate
+        #       `bins_to_seconds` method for convenience. Same with chunksize.
+        #       (so that ideally `fs` isn't needed at all, except as an arg to
+        #        the convenience method).
         # convert u & tau units from sec to bins
-        taui = taui * self.fs
-        ui = ui / self.fs * 100
+        taui = tau * self.fs
+        ui = u / self.fs * 100
 
         # convert chunksize from sec to bins
         chunksize = int(self.chunksize * self.fs)
@@ -112,38 +161,31 @@ class ShortTermPlasticity(Layer):
             # assumes dim of u is 1 !
             tstim = np.mean(tstim, axis=0, keepdims=True)
         if len(ui.shape) == 1:
-            ui = np.expand_dims(ui, axis=1)
-            taui = np.expand_dims(taui, axis=1)
+            ui = ui[..., np.newaxis]      # ~20x faster than expand_dims
+            taui = taui[..., np.newaxis]
 
-        #ui=ui.T
-        #taui=taui.T
+        for i in range(u.shape[0]):
 
-        for i in range(0, len(u)):
             if self.quick_eval:
-
                 a = 1 / taui
                 x = ui * tstim
+                # TODO: different way to accomplish this? Creating array of
+                #       ones for the full stimulus shape is rather slow.
+                td = np.ones(shape=s)
+                x0, imu0 = 0.0, 0.0
 
-                if self.reset_signal is None:
-                    reset_times = np.arange(0, s[1] + chunksize - 1, chunksize)
-                else:
-                    reset_times = np.argwhere(self.reset_signal[0, :])[:, 0]
-                    reset_times = np.append(reset_times, s[1])
-
-                td = np.ones_like(x)
-                x0, imu0 = 0., 0.
                 for j in range(len(reset_times) - 1):
                     si = slice(reset_times[j], reset_times[j + 1])
                     xi = x[:, si]
 
-                    ix = _cumtrapz(a + xi, dx=1, initial=0, axis=1) + a + (x0 + xi[:, :1]) / 2
+                    ix = self.cumulative_integral_trapz(a + xi, dx=1, initial=0, axis=1) + a + (x0 + xi[:, :1]) / 2
 
-                    mu = np.exp(ix)
-                    imu = _cumtrapz(mu * xi, dx=1, initial=0, axis=1) + (x0 + mu[:, :1] * xi[:, :1]) / 2 + imu0
+                    mu = ne.evaluate('exp(ix)')
+                    imu = self.cumulative_integral_trapz(mu * xi, dx=1, initial=0, axis=1) + (x0 + mu[:, :1] * xi[:, :1]) / 2 + imu0
 
                     ff = np.bitwise_and(mu > 0, imu > 0)
                     _td = np.ones_like(mu)
-                    _td[ff] = 1 - np.exp(np.log(imu[ff]) - np.log(mu[ff]))
+                    _td[ff] = ne.evaluate('1 - exp(log(imu[ff]) - log(mu[ff]))')
                     td[:, si] = _td
 
                     x0 = xi[:, -1:]
@@ -195,6 +237,45 @@ class ShortTermPlasticity(Layer):
 
         return stim_out.T
 
+    @staticmethod
+    def cumulative_integral_trapz(y, dx=1.0, initial=0.0, axis=1):
+        """Cumulative integral of y(x) using the trapezoid method.
+        
+        Compare to `scipy.integrate.cumtrapz`. This method is slightly faster,
+        likely due to supporting fewer options.
+        
+        Parameters
+        ----------
+        y : np.ndarray.
+            Values to integrate.
+        dx : int; default=1.0.
+            Spacing between elements of `y`.
+        axis : int; default=1.
+            Specifies the axis over which to compute the cumulative sum.
+        initial : float; default=0.0.
+            Use this as the first value in the returned array.
+
+        Returns
+        -------
+        np.ndarray
+            Integrated values, with the same shape as `y`.
+        
+        """
+        # TODO: profile memory usage.
+        # TODO: move this to nems.tools?
+        y = (y[:, :-1] + y[:, 1:]) / 2.0
+        # TODO: Is the padding ever *not* done on the time axis? If not,
+        #       concat is ~10x faster and does the same thing.
+        #       With this version, even a little faster than
+        #       `scipy.integrate.cumtrapz` and does the same thing.
+        y = np.concatenate([[[initial]], y], axis=1)
+        # x = np.pad(
+        #     x, ((0, 0), (1, 0)), 'constant', constant_values=(initial, initial)
+        #     )
+        y = np.multiply(np.cumsum(y, axis=axis), dx, out=y)
+
+        return y
+
     @layer('stp')
     def from_keyword(keyword):
         """TODO: docs"""
@@ -215,16 +296,29 @@ class ShortTermPlasticity(Layer):
         TODO: docs"""
         import tensorflow as tf
         from nems.backends.tf import NemsKerasLayer
-
-        #u, tau = self.get_parameter_values()
-        #u = u.astype('float32')
-        #tau = tau.astype('float32')
+    
 
         parent_x0 = self.x0
         fs = self.fs
         crosstalk = self.crosstalk
+        if crosstalk:
+            # TODO: Remove this after implementing crosstalk
+            raise NotImplemented(
+                'STP(..., crosstalk=True) is not yet supported for the '
+                'TensorFlow backend.'
+                )
         parent_chunksize = self.chunksize
         reset_signal = self.reset_signal
+
+        @tf.function
+        def _cumtrapz(x, dx=1., initial=0.):
+            x = (x[:, :-1] + x[:, 1:]) / 2.0
+            x = tf.pad(
+                x, ((0, 0), (1, 0), (0, 0)), constant_values=initial
+                )
+
+            return tf.cumsum(x, axis=1) * dx
+
 
         class ShortTermPlasticityTF(NemsKerasLayer):
             def call(self, inputs):
@@ -250,18 +344,11 @@ class ShortTermPlasticity(Layer):
                 chunksize = int(parent_chunksize * fs)
 
                 if crosstalk:
-                    raise NotImplemented
                     # assumes dim of u is 1 !
                     tstim = tf.math.reduce_mean(tstim, axis=0, keepdims=True)
 
                 ui = tf.expand_dims(ui, axis=0)
                 taui = tf.expand_dims(taui, axis=0)
-
-                @tf.function
-                def _cumtrapz(x, dx=1., initial=0.):
-                    x = (x[:, :-1] + x[:, 1:]) / 2.0
-                    x = tf.pad(x, ((0, 0), (1, 0), (0, 0)), constant_values=initial)
-                    return tf.cumsum(x, axis=1) * dx
 
                 a = tf.cast(1.0 / taui, 'float64')
                 x = ui * tstim
@@ -270,16 +357,23 @@ class ShortTermPlasticity(Layer):
                     reset_times = tf.range(0, s[1] + chunksize - 1, chunksize)
                 else:
                     reset_times = tf.where(reset_signal[0, :])[:, 0]
-                    reset_times = tf.pad(reset_times, ((0, 1),), constant_values=s[1])
+                    reset_times = tf.pad(
+                        reset_times, ((0, 1),), constant_values=s[1]
+                        )
 
                 td = []
                 x0, imu0 = 0.0, 0.0
                 for j in range(reset_times.shape[0] - 1):
-                    xi = tf.cast(x[:, reset_times[j]:reset_times[j + 1], :], 'float64')
-                    ix = _cumtrapz(a + xi, dx=1, initial=0) + a + (x0 + xi[:, :1]) / 2.0
+                    xi = tf.cast(
+                        x[:, reset_times[j]:reset_times[j + 1], :],
+                        'float64'
+                        )
+                    ix = _cumtrapz(a + xi, dx=1, initial=0) \
+                         + a + (x0 + xi[:, :1]) / 2.0
 
                     mu = tf.exp(ix)
-                    imu = _cumtrapz(mu * xi, dx=1, initial=0) + (x0 + mu[:, :1] * xi[:, :1]) / 2.0 + imu0
+                    imu = _cumtrapz(mu * xi, dx=1, initial=0) \
+                          + (x0 + mu[:, :1] * xi[:, :1]) / 2.0 + imu0
 
                     valid = tf.logical_and(mu > 0.0, imu > 0.0)
                     mu = tf.where(valid, mu, 1.0)
@@ -292,20 +386,15 @@ class ShortTermPlasticity(Layer):
                     td.append(tf.cast(_td, 'float32'))
                 td = tf.concat(td, axis=1)
 
-                # ret = tstim * td
-                # offset depression by one to allow transients
-                ret = tstim * tf.pad(td[:, :-1, :], ((0, 0), (1, 0), (0, 0)), constant_values=1.0)
+                ret = tstim * tf.pad(
+                        td[:, :-1, :], ((0, 0), (1, 0), (0, 0)),
+                        constant_values=1.0
+                        )
                 ret = tf.where(tf.math.is_nan(inputs), _nan, ret)
 
                 return ret
 
-            #def weights_to_values(self):
-            #    values = self.parameter_values
-            #    # TODO?? Remove extra dummy axis if one was added, and undo scaling.
-            #    return values
-
         return ShortTermPlasticityTF(self, **kwargs)
-
 
 
 class STP(ShortTermPlasticity):
