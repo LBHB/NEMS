5e6505033c2802d8f34ccfd993d914362d0d5389
jacobpennington <jacob.pennington@wsu.edu>
2022-10-21 11:28:19 -0700

    added more stp pytests

--- tests/layers/test_stp.py
+++ tests/layers/test_stp.py
@@ -1,7 +1,10 @@
 import pytest
 import numpy as np
 
+from nems import Model
 from nems.layers.stp import ShortTermPlasticity
+from nems.visualization.layers import stp_test_input
+
 
 def test_constructor():
     with pytest.raises(TypeError):
@@ -11,36 +14,43 @@ def test_constructor():
     stp = ShortTermPlasticity(shape=(2,1))
     
 
-"""
-import numpy as np
-import matplotlib.pyplot as plt
-import tensorflow as tf
+class TestEvaluate:
 
-from nems.layers.stp import ShortTermPlasticity
-stp = ShortTermPlasticity(shape=(3,1), fs=100)
-input = np.random.randn(100,3)
-input[:,1]=0
-input[10:20,1]=1
-input[25:35,1]=1
-input[60:90,1]=1
-input[:,2]=0
-tf_input = tf.constant(input[np.newaxis,...].astype(np.float32))
-tf_stp = stp.as_tensorflow_layer()
+    def test_one(self, spectrogram):
+        time, spectral = spectrogram.shape
+        spectrogram[spectrogram < 0] = 0  # STP only supports >= 0 for now.
+        model = Model.from_keywords(f'wc.{spectral}x1-stp.1')
+        out = model.predict(spectrogram)
+        assert out.shape == (time, 1)
 
-output=stp.evaluate(input)
-tf_output = tf_stp.call(tf_input).numpy().squeeze(axis=0)
+    def test_full(self, spectrogram):
+        time, spectral = spectrogram.shape
+        stp = ShortTermPlasticity(shape=(18,))
+        out = stp.evaluate(spectrogram)
+        assert out.shape == spectrogram.shape
 
-f,ax=plt.subplots(3,1)
-ax[0].plot(input[:,0])
-ax[0].plot(output[:,0])
-ax[0].plot(tf_output[:,0])
-ax[1].plot(input[:,1])
-ax[1].plot(output[:,1])
-ax[1].plot(tf_output[:,1])
+    def test_quick(self, spectrogram):
+        time, spectral = spectrogram.shape
+        stp = ShortTermPlasticity(shape=(spectral,), quick_eval=True)
+        out = stp.evaluate(spectrogram)
 
-ax[2].plot(output-tf_output)
+    def test_dep_only(self, spectrogram):
+        time, spectral = spectrogram.shape
+        stp = ShortTermPlasticity(shape=(spectral,), dep_only=True)
+        out = stp.evaluate(spectrogram)
+        assert out.max() <= spectrogram.max()
 
+    def test_input(self):
+        # 0: randomm, 1: square, 2: all zero
+        input = stp_test_input()
+        stp = ShortTermPlasticity(shape=(3,))
+        u, t = stp.get_parameter_values()
+        # default should be depression
+        assert np.all(u > 0)
 
-np.allclose(output, tf_output)
-"""
-
+        output = stp.evaluate(input)
+        # Depression should never increase 
+        assert output.max() <= input.max()
+        assert output.min() <= input.min()
+        # All-zero input should be unchanged
+        assert np.allclose(input[:, 2], output[:, 2])
