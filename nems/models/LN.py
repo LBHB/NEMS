185b491e8779a8a92af104812c9beea64f051439
jacobpennington <jacob.pennington@wsu.edu>
2022-10-21 19:54:24 -0700

    Added pre-built LN_STRF

--- nems/models/LN.py
+++ nems/models/LN.py
@@ -1,25 +1,96 @@
 from .base import Model
+from nems.layers import (
+    WeightChannels, WeightChannelsGaussian, FiniteImpulseResponse,
+    RectifiedLinear, DoubleExponential
+    )
 
 class LN_STRF(Model):
-    '''
-    A ModelSpec with the following modules:
-        1) WeightChannels(shape=(4, n_channels), parameterization='gaussian')
-        2) FIR(shape=(4, 15), parameterization='P3Z1')
-        3) LevelShift(),
-        4) DoubleExponential()
 
-    Based on the best-performing model from
-    Thorson, Lienard and David (2015)
-    doi: 10.1371/journal.pcbi.1004628
+    def __init__(self, channels, time_bins, rank=None,
+                 gaussian=True, nonlinearity='DoubleExponential',
+                 nl_kwargs=None, **model_init_kwargs):
+        """Linear-nonlinear Spectro-Temporal Receptive Field model.
 
-    '''
+        Contains the following layers:
+        1. WeightChannels, WeightChannelsGaussian, or None (if full rank).
+        2. FiniteImpulseResponse
+        3. DoubleExponential, RectifiedLinear, or another static nonlinearity.
 
-    def __init__(n_channels):
-        # Need to know number of spectral channels in the stimulus
-        self.n_channels = n_channels
+        Expects a single sound spectrogram as input, with shape (T, N), and a
+        single recorded neural response as a target, with shape (T, 1), where
+        T and N are the number of time bins and spectral channels, respectively.
 
-    # TODO: everything else, this is just to illustrate the idea.
+        Based on model architectures described in:
+        Thorson, Lienard and David (2015)
+        doi: 10.1371/journal.pcbi.1004628
 
+        Parameters
+        ----------
+        channels : int.
+            Number of spectral channels in spectrogram.
+        time_bins : int.
+            Number of "taps" in FIR filter. We have found that a 150-250ms filter
+            typically sufficient for modeling A1 responses, or 15-25 bins for
+            a 100 Hz sampling rate.
+        rank : int; optional.
+            Number of spectral weightings used as input to a reduced-rank filter.
+            For example, `rank=1` indicates a frequency-time separable STRF.
+            If unspecified, a full-rank filter will be used.
+        gaussian : bool; default=True.
+            If True, use gaussian functions (1 per `rank`) to parameterize
+            spectral weightings. Unused if `rank is None`.
+        nonlinearity : str; default='DoubleExponential'.
+            Specifies which static nonlinearity to apply after the STRF.
+            Default is the double exponential nonlinearity used in the paper
+            cited above.
+        nl_kwargs : dict; optional.
+            Additional keyword arguments for the nonlinearity Layer, like
+            `no_shift` or `no_offset` for `RectifiedLinear`.
+        model_init_kwargs : dict; optional.
+            Additional keyword arguments for `Model.__init__`, like `dtype`
+            or `meta`.
+
+        """
+
+        super().__init__(**model_init_kwargs)
+
+        # Add STRF
+        if rank is None:
+            # Full-rank finite impulse response
+            fir = FiniteImpulseResponse(shape=(time_bins, channels))
+            self.add_layers(fir)
+        else:
+            wc_class = WeightChannelsGaussian if gaussian else WeightChannels
+            wc = wc_class(shape=(channels, rank))
+            fir = FiniteImpulseResponse(shape=(time_bins, rank))
+            self.add_layers(wc, fir)
+
+        # Add static nonlinearity
+        if nonlinearity in ['DoubleExponential', 'dexp', 'DEXP']:
+            nl_class = DoubleExponential
+        elif nonlinearity in ['RectifiedLinear', 'relu', 'ReLU']:
+            nl_class = RectifiedLinear
+        else:
+            raise ValueError(
+                f'Unrecognized nonlinearity for LN model:  {nonlinearity}.')
+        
+        if nl_kwargs is None: nl_kwargs = {}
+        nonlinearity = nl_class(shape=(1,), **nl_kwargs)
+        self.add_layers(nonlinearity)
+
+
+    @classmethod
+    def from_data(cls, input, filter_duration, sampling_rate=1000, **kwargs):
+        channels = input.shape[-1]
+        time_bins = int(filter_duration/1000 * sampling_rate)
+        # TODO: modify initial parameters based on stimulus statistics?
+        return LN_STRF(channels, time_bins, **kwargs)
+
+    def fit_LBHB():
+        # TODO: 3-stage fit with freezing/unfreezing NL
+        pass
+
+    # TODO
     # @module('LNSTRF')
     def from_keyword(keyword):
         # Return a list of module instances matching this pre-built Model?
