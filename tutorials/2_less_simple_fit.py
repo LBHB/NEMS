a002917f491f7ef0192e82372edc0afde0ecca8e
Najir <irrperks@gmail.com>
2023-06-29 11:29:30 -0700

    Added constants for Time/Channels, created a response target that is relative to our spectrogram, set up temp plots for inputs. Reformatted and cleaned up comments to readability and clarity

--- tutorials/2_less_simple_fit.py
+++ tutorials/2_less_simple_fit.py
@@ -1,6 +1,7 @@
 """Demonstrates fitting Layers with multiple inputs, and Layer subclassing."""
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 from nems import Model
 from nems.layers import WeightChannels, FIR, DoubleExponential
@@ -9,18 +10,38 @@ from nems.layers.base import Layer, Phi, Parameter
 # Fast-running toy fit options for demonstrations.
 options = {'options': {'maxiter': 2, 'ftol': 1e-2}}
 
+########################################################
+# Creating custom layers
+# Sometimes you may need to deal with more data that our Inp, Out, and State
+# We can create our own layers built from our base layer, to include that new data
+#   initial_parameters: 
+#   evaluate: 
+# See more at: https://temp.website.net/nems_layers_base_layer
+########################################################
 
-# Some models will need more data than input, output, and state. In that case,
-# the fit process requires a couple of extra steps. First, we'll define some
-# simple example Layers that require additional inputs and then load data again.
-
-# Dummy layer that can make use of an arbitrary number of inputs.
+###########################
+# Sum Layer, dummy layer that allows arbitrary number of inputs
+#   evaluate: Allows you to specify wanted inputs, and define potential operations
+#       *inputs: Any number of data, gets pushed into a flat evaluation with no operation
+# See more at: https://temp.website.net/nems_layers_base_Layer_evaluate
+###########################
 class Sum(Layer):
     def evaluate(self, *inputs):
         # All inputs are treated the same, no fittable parameters.
         return np.sum(inputs)
 
-# Dummy layer that makes use of its inputs in different ways.
+###########################
+# Linear Weighting Layer, dummy layer that allows some manipulation of provided data
+#   initial_parameters: Allows you to evoke a set of parameters that are passed to layers.parameter
+#       a: a parameter value of shape(1,)
+#       b: a second parameter value of shape(1,)
+#       output: Phi(a, b)
+#   evaluate: Same as before, In this case returns a multiplication of some set of inputs to parameters
+#       a,b: provided parameters initialized in our first function
+#       prediction, pupil, other_state: 3 various inputs
+#       output: parameter1*input1 + parameter2*input2 + input3
+# See more at: https://temp.website.net/nems_layers_base_Layer_initialparameters
+###########################
 class LinearWeighting(Layer):
 
     def initial_parameters(self):
@@ -34,50 +55,54 @@ class LinearWeighting(Layer):
         a, b = self.get_parameter_values('a', 'b')
         return a*prediction + b*pupil + other_state
 
-
 def my_complicated_data_loader(file_path):
-    # Dummy function to demonstrate the data format.
     print(f'Loading data from {file_path}, but not really...')
-    spectrogram = np.random.rand(100, 18)
-    response = np.random.rand(100, 5)
-    pupil = np.random.rand(100, 1)
-    other_state = np.random.rand(100, 1)
 
-    return spectrogram, response, pupil, other_state
+    # TIME = Representation of some x time to use in our layers
+    # CHANNELS = Representation of some y channels for # of inputs in our layers
+    TIME = 100
+    CHANNELS = 18
 
-stim, resp, pupil, state = my_complicated_data_loader('/path/data.csv')
+    # Creation of random 2D numpy array with X time representation and Y channel representations
+    spectrogram = np.random.rand(TIME, CHANNELS)
+    # Using our Spectrogram to create a target set of data that our model will attempt to fit
+    response = np.stack(spectrogram[:, :5])
+    # An example of using states to fit our model together
+    pupil_size = np.random.rand(TIME, 1)
+    # Some third input to test our custom function that takes a second state
+    other_state = np.random.rand(TIME, 1)
 
+    return spectrogram, response, pupil_size, other_state, TIME, CHANNELS
 
-# For a model that uses multiple inputs, we package the input data into
-# a dictionary. This way, layers can specify which inputs they need using the
-# assigned keys.
+# Create variables from our data import function
+stim, resp, pupil, state, TIME, CHANNELS = my_complicated_data_loader('/path/data.csv')
+
+###########################
+# A model with multiple inputs requires a Dict of keywords/values ex,
+#   {
+#       keyword1: input1,
+#       keyword2: input2,
+#       keyword3: input3,
+#       etc....
+#   }
+###########################
 input = {'stimulus': stim, 'pupil': pupil, 'state': state}
 target = resp
 
 
-# Now we build the Model as before, but we can specify which Layer receives
-# which input(s) during fitting. We'll also use a factorized, parameterized STRF
-# in place of the full-rank version to demonstrate usage. For layers that only
-# need a single input, we provide the approprtiate dictionary key. In this case,
-# we named the spectrogram data 'stimulus' so that will be the input to
-# `WeightChannels`. The inputs for `FIR` and `DoubleExponential` should be the
-# outputs of their preceding layers, so we don't need to specify an input.
+###########################
+# Layers for our model, This time in the form of a list of functions
+# List can then be passed to our model instead of via .add_layers()
 #
-# We do want to track the output of the LN portion of the model
-# (`WeightChannels`, `FIR`, and `DoubleExponential`) separately from the rest,
-# so we specify `output='LN_output'` for `DoubleExponential`.
-#
-# We want to apply the `Sum` layer to the output of the LN portion of the
-# model and to both of our state variables. The order of the data variables
-# doesn't matter for this layer, so we provide a list of the dictionary keys.
-#
-# Order does matter for the `LinearWeighting` layer, so we provide a dictionary
-# instead to ensure the correct data is mapped to each parameter
-# for `LinearWeighting.evaluate`.
-# NOTE: We could also have used a list as long as the keys were specified in
-#       the same order as in the method definition. However, using a dictionary
-#       clarifies what the data is mapped to without needing to refer back to
-#       the Layer implementation.
+# Setting up layers in this way allows you to specify inputs for specific layers
+#   WeightedChannels: Takes 'stimulus' from dict as an input
+#   FIR: Takes the preceding layer as it's input
+#   Double Exp: Also takes preceding layer as input, but outputs LN for future inputs
+#   Sum: Adds up a bunch of inputs including the LN output
+#   LinearWeighting: Sets evaluation parameters from dict keys
+#       Also keeps parameters empty, so they are infered from other layers(DOUBLE CHECK THIS)
+# See more at: https://temp.website.net/model_layers
+###########################
 layers = [
     WeightChannels(shape=(18,4), input='stimulus'),
     FIR(shape=(15, 4)),
@@ -96,5 +121,25 @@ model = Model(layers=layers)
 # We fit as before, but provide the `input` dictionary in place of individual
 # variables. The necessary inputs are already specified in the layers, so we
 # only need to tell the model what data to match its output to (`resp`).
-model.fit(input=input, target=resp, fitter_options=options)
+fitted_model = model.fit(input=input, target=resp, fitter_options=options)
 prediction = model.predict(input)
+
+# plots our fitted model, and graphs of raw data before model fitting
+fitted_model.plot(input=input, target=resp, figure_kwargs={'figsize': (12,8)})
+
+# Plots ~7 stimulus channels, state, and pupil inputs created in our fake dataset before model uses them
+raw_plot, ax = plt.subplots(3, 3, figsize=(12,8))
+for i in range(0, 7):
+    axes = ax[int(np.ceil(i/3)-1)][i%3]
+    axes.plot(range(0,TIME), (input['stimulus'][:, i]*10).astype(int))
+    axes.set_title(f"Stimulus Channel {i}")
+
+axes = ax[2][1]
+axes.plot(range(0,TIME), (input['state'][:, :]*10).astype(int))
+axes.set_title("State")
+
+axes = ax[2][2]
+axes.plot(range(0,TIME), (input['pupil'][:, :]*10).astype(int)) 
+axes.set_title("Pupil")
+plt.show()
+
