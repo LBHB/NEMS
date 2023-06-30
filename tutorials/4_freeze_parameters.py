18b013be914647dac569348d890c55544eeeee86
Najir <irrperks@gmail.com>
2023-06-29 17:16:07 -0700

    Reformatted comments and information for readability. Changed target to be relative to input. Added another print on parameters for comparison at the end

--- tutorials/4_freeze_parameters.py
+++ tutorials/4_freeze_parameters.py
@@ -5,19 +5,30 @@ import numpy as np
 from nems import Model
 from nems.layers import WeightChannels, FIR, DoubleExponential
 
+########################################################
+# Freezing Parameters
+# When fitting models we've found that seperating linear and non-linear steps
+# will improve the fit
+#
+# We have a variety of ways to freeze certain parts of our model
+#   - Freezing Linear/Non-linear parameters in all layers
+#   - Freezing specific layers or a list of layers
+#   - Freezing specific parameters within any specific layer
+#
+# Note: It will be helpful to keep track of your layers index and/or provide names
+########################################################
 
-# In the past, we've found that our models predict our data best if we split
-# the fitting process into linear and nonlinear steps. We can do this with
-# NEMS with `StaticNonlinearity.skip_nonlinearity()` and repeated `model.fit()`.
-# NOTE: We will use the `maxiter` option for `scipy.optimize.minimize` to run
-#       the script in a reasonable amount of time. For a real fit, a much larger
-#       number should be used (see `scipy` documentation for defaults`).
-
-# "Load" the data
+# Creating fake data, see "1_simple_fit" tutorial for more information
 input = np.random.rand(1000, 18)  # 18-channel spectrogram stimulus
-target = np.random.rand(1000, 1)   # PSTH response of a single neuron
+target = np.stack(input[:, :5])   # PSTH response of a single neuron
 
-# Build a simple linear model.
+###########################
+# Building a standard linear model
+#
+# - Layers have parameter name="some_name"
+#   - This will be used for freezing specific layers
+# - Double Exponential shape=(x,) creates a non-linear layer
+###########################
 model = Model()
 model.add_layers(
     WeightChannels(shape=(18,4), name='wc'),
@@ -25,13 +36,11 @@ model.add_layers(
     DoubleExponential(shape=(1,), name='dexp'),
 )
 
-# Fit the linear model using a coarse tolerance.
-# NOTE: The nested dictionary structure is due to the syntax for
-#       `scipy.optimize.minimize` kwargs, see their documentation.
+# Options parameters for demonstration, see more at: scipy.optimize.minimize
 initialization = {'options': {'ftol': 1e3, 'maxiter': 10}}
-# This will freeze (make untrainable) all nonlinear parameters of
-# DoubleExponential, but still fit the `shift` prameter (which effectively
-# results in a scalar offset: `output = input + shift`).
+
+# Layer function will skip non-linear parameters, while still modifying linear ones
+# for example: this results in a scalar offset: `output = input + shift`).
 model.layers['dexp'].skip_nonlinearity()
 
 # Show the number of frozen (untrainable), unfrozen (trainable),
@@ -42,25 +51,29 @@ print(f'Parameters for first fit:\n{model.parameter_info}\n')
 # the same model name (i.e. the unfit model will be overwritten).
 model = model.fit(input=input, target=target, fitter_options=initialization)
 
-# Add the nonlinear component to the end of the model.
+# Opens our non-linear paramenters back up to modification again
 model.layers['dexp'].unskip_nonlinearity()
 
-# Freeze the parameters of the linear portion of the model.
-# The values of these parameters will not be changed during fitting.
+###########################
+# Freezing groups of layers
+# 
+# Often we will need to activate large groups of layers or parameters
+#   - Integer: We can do this my providing indexs of layers
+#   - Keyword: We can use our defined names for layers instead
+###########################
 model.freeze_layers('wc', 'fir')
-# model.freeze_layers(0, 1)  # integer indexing would also work
-
 print(f'\nParameters for second fit:\n{model.parameter_info}\n')
 
 # Fit the model again, using the previous fit as a starting point.
 model = model.fit(input=input, target=target, fitter_options=initialization)
 
-# Unfreeze the linear portion.
+###########################
+# Unfreezing Layers
+#
+# Follows the same pattern as freezing in addtion;
+#   - unfreeze_layers() will unfreeze every layer
+###########################
 model.unfreeze_layers('wc', 'fir')
-# In this case, this is equivalent to unfreezing all layers, which we can
-# do by *not* specifying any names or indices:
-# model.unfreeze_layers()
-
 print(f'\nParameters for final fit:\n{model.parameter_info}\n')
 
 # Now perform a final fit of all parameters simultaneously, using a finer
@@ -74,3 +87,4 @@ model = model.fit(input=input, target=target, fitter_options=final_fit)
 #       For example:
 # Just freeze DoubleExponential's kappa parameter:
 model.layers['dexp'].freeze_parameters('kappa')
+print(f'\nParameters for final fit:\n{model.parameter_info}\n')
