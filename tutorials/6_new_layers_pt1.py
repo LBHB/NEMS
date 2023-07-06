35797c8aad0a7af7e99bd5768a1f5e41de0beffd
Najir <irrperks@gmail.com>
2023-07-06 08:48:15 -0700

    Cleaned up & refactored code to make it more readable and informative. Added in a few graphs and printouts to show important info. reworded some variables for readability. Added in model init keywords for layers example

--- tutorials/6_new_layers_pt1.py
+++ tutorials/6_new_layers_pt1.py
@@ -1,21 +1,34 @@
 """Demonstrates the key steps needed to create new Layer subclasses."""
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 from nems import Model
 from nems.distributions import Normal, HalfNormal
 from nems.registry import layer
 from nems.layers.base import Layer, Phi, Parameter
 
-
-# Creating a new Layer subclass is the first step in expanding NEMS to include
-# new types of models. Most new subclasses will need to define two methods:
-# 1) `initial_parameters`
-#    Specifies the Layer's trainable Parameters and default bounds & priors.
-# 2) `evaluate`
-#    Defines the transformation that the Layer applies to inputs.
-
+########################################################
+# Creating new layers
+#
+# Using Layer as a parent, we can create new layer subclasses for your own usecases.
+# like any subclass, a newly created layer will inherit many features, so make sure
+# to look at the base layer function too.
 # As an example, we'll define applies exponential decay to its input.
+#   - initial_parameters: Lets you specify trainable parameters, bounds, or priors
+#       - s: Parameter(), provide name, shape, prior, bounds etc... 
+#       - t: another Parameter()
+#       - Phi: Returned 'List' of parameters and info
+#   - evaluate: Defines the tranformation applied to inputs
+#       - evaluate(self, input): need at least one argument, but can specify more
+#       - s,t: recieve paramter values from initial_parameters
+#       - Returns data after operations have been performed
+#   - from_keyword(keyword): 
+#       - @layer('name'): Decorator for setting up keyword layers
+#       - keyword: Refered by the decorator, could be used for a parser or other modifications
+#       - Returns an instance of our new layer
+# See more at: https://temp.website.net/nems_base_Layer
+########################################################
 class ExponentialDecay(Layer):
 
     def initial_parameters(self):
@@ -52,26 +65,63 @@ decay = ExponentialDecay()
 
 # Now we can use our new Layer to:
 
-# 1) Transform data:
-x = np.arange(1000)[..., np.newaxis]
-y = decay.evaluate(x)
+###########################
+# Set up our data
+#   input: Linear set of data with length 1000
+#   target: random non-linear decay dataset to represent decay
+#   y: Output of our input being thrown directly into our new layer
+###########################
+input = np.arange(1000)[..., np.newaxis]
+target = -6*np.exp(-1/100*input) + np.random.rand(1000, 1)
+y = decay.evaluate(input)
 
-# 2) Sample new parameter values:
+# Here is a comparison of our values before we run any models
+print(f"""
+Our first inital input is: {input[0]}\n
+Our first target is: {target[0]}\n
+The first output of our decay layer with no adjustments or fitting is: {y[0]}""")
+
+###########################
+# Sample new parameter values
+# sample_from_priors: creates layer with new parameter values sampled from priors
+#   inplace: When true, update and adjust parameters inplace
+###########################
 decay.sample_from_priors(inplace=True)
 
-# 3) Build models:
-model1 = Model()
-model1.add_layers(decay)
+###########################
+# Building models
+#   via list of layers
+#   direct model initialization
+#   from_keywords() list
+###########################
+model = Model()
+model.add_layers(decay)
+model = Model('decay')
 # NOTE: `Model.from_keywords` will use a *new* instance of ExponentialDecay.
-model2 = Model.from_keywords('decay')
+model = Model().from_keywords('decay')
 
-# 4) Optimize parameters:
-z = -6*np.exp(-1/100*x) + np.random.rand(1000, 1) # Target
-fitted = model2.fit(
-    x, target=z,
+###########################
+# Fit our model to optimize parameters
+#   fitter_options: Same as other tutorials, used for demonstration
+###########################
+fitted_model = model.fit(
+    input=input, target=target,
     # Set low iteration count to speed up fitting demonstration.
     fitter_options={'options': {'maxiter': 50, 'ftol': 1e-10}}
     )
 
-# Plot results:
-fitted.plot(input=x, target=z)
+###########################
+# Predict our model
+###########################
+predicted_model = model.predict(input=target)
+
+###########################
+# Plots results
+# This will show 2 simple graphs
+#   Our linear input graph
+#   Our randomized adjustment graph
+#   A second line on our second graph for the output of our decay model
+###########################
+fitted_model.plot(input=input, target=target)
+
+plt.show()
