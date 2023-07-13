2cd674aac8baa21cc214864aa8b7d7dfb8d9293c
Isaac Perks <irrperks@gmail.com>
2023-07-13 10:46:30 -0700

    Added info for parameters. Rewrote loop to add more info and be more effecient. Printed out more information on models

--- tutorials/4_backend_fits.py
+++ tutorials/4_backend_fits.py
@@ -25,15 +25,26 @@ from nems import visualization
 ########################################################
 
 ###########################
-# Setting up Demo Data, see auditory data tutorial
+# Setting up Demo Data instead of dummy data
+# 
+# load_demo(): Provides our tuple of training/testing dictionaries
+#   This time we will be loading a different data set, and finding a 
+#   specific cell that contains data worth testing
+#
+#   "TAR010c_data.npz": The data parameter to load our different dataset
+#   [:, [cid]]: Splitting the dataset to get a specific cells data
+#   ['cellid'][cid]: Get cellid at cid for labeling and f+ormatting
 ###########################
 nems.download_demo()
-training_dict, test_dict = nems.load_demo()
+training_dict, test_dict = nems.load_demo("TAR010c_data.npz")
 
+cid=29 # Picking our cell to pull data from
+cellid = training_dict['cellid'][cid]
 spectrogram_fit = training_dict['spectrogram']
-response_fit = training_dict['response']
+response_fit = training_dict['response'][:,[cid]]
 spectrogram_test = test_dict['spectrogram']
-response_test = test_dict['response']
+response_test = test_dict['response'][:,[cid]]
+
 
 # Creating a basic Rank2LNSTRF model to test parameters on
 model = Model()
@@ -47,20 +58,26 @@ model.add_layers(
 # Picking your parameters
 #
 # Currently we have 2 core backends available, with
-# their own parameters and options. 
+# their own parameters and options. There are a lot
+# of available parameters,
+# See more info at: nems.backends.{backend}._fit
+#
+#   model.fit()
+#       - fitter_options: Lets you provide a dictionary of options for the fitter
+#       - backend: Specify which backend you will be using
 #
 #   tf_options:
-#       - cost_function:
-#       - early_stopping:
-#           - delay:
-#           - patience:
-#           - tolerance:
-#       - validation_split:
-#       - learning_rate:
-#       - epochs: 
+#       - cost_function: Function used for determining the error rate of our model
+#       - early_stopping: Determining when and how to stop your model early to avoid overfitting
+#           - delay: Min epochs run before early-stopping is possible
+#           - patience: How many epochs to iterate without improvement before we end early
+#           - tolerance: Min change in error to be considered improvement
+#       - validation_split: Ratio to split our data into testing and validation sets 
+#       - learning_rate: The scale your weights will adjust each iteration
+#       - epochs: Number of time we want to train our model on a given dataset
 #   scipy:
-#       - maxiter:
-#       - ftol: 
+#       - maxiter: Maximum iterations to be performed
+#       - ftol: Float-Point precision to stop our iterations at
 #   
 ###########################
 backend = 'scipy'
@@ -84,9 +101,10 @@ else:
 #   - N: The number of models you want to create
 ###########################
 N = 5
-sample_array = model.sample_from_priors(N)
-fitted_array = []
+sample_list = model.sample_from_priors(N)
+fitted_list = []
 best_fit = None
+r_test_list = []
 
 # We also want to print out each of our models predictions on the same data, to compare
 f, ax = plt.subplots(N+2, 1, sharex='col')
@@ -96,20 +114,24 @@ ax[1].plot(response_test, label='actual response')
 ax[1].set_ylabel('Test response')
 
 # This loops fits each model, compares them to find best fit, and shows how they compare on a graph
-iter = 0
-for x in sample_array:
-    fitted_array.append(x.fit(spectrogram_fit, response_fit))
-    pred_model = x.predict(spectrogram_test)
-    if best_fit is None or best_fit.results.final_error > fitted_array[iter].results.final_error:
-        best_fit = fitted_array[iter]
+# In practice you will want to print entire models, not just predictions, to find your best model
+for fitidx, cnn_model in enumerate(sample_list):
+    cnn_model.name = f"{cellid}_CNN_fit-{fitidx}"
+    fitted_list.append(cnn_model.fit(spectrogram_fit, response_fit, backend=backend, fitter_options=options))
+    pred_cnn = cnn_model.predict(spectrogram_test)
+    r_test_list.append(np.corrcoef(pred_cnn[:, 0], response_test[:, 0])[0, 1])
+    if best_fit is None or best_fit.results.final_error > fitted_list[fitidx].results.final_error:
+        best_fit = fitted_list[fitidx]
 
-    ax[iter+2].plot(pred_model, label='predicted')
-    ax[iter+2].set_ylabel(f'fit {iter}')
-    iter += 1
+    ax[fitidx+2].plot(pred_cnn, label='predicted')
+    ax[fitidx+2].set_ylabel(f'fit {fitidx}')
 
 # Plotting the model of our best fit
-best_fit.name = "Best Fit"
+best_fit.name = f"Best Fit, #{fitidx}"
 best_fit.plot(spectrogram_test, target=response_test)
+
+for fitidx, cnn in enumerate(fitted_list):
+    print(f"CNN fit {fitidx} final E={cnn.results.final_error:.3f} r test={r_test_list[fitidx]:.3f}")
 
 ## Uncomment if you don't have an interactive backend installed
 #plt.show()
