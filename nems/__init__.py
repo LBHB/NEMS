51a618f1c05e68d82662bed5d444af5d32052512
Isaac Perks <irrperks@gmail.com>
2023-07-13 15:02:45 -0700

    Packaging for model_list

--- nems/__init__.py
+++ nems/__init__.py
@@ -4,6 +4,6 @@ Ex: `from nems import Model`
 
 """
 
-from nems.models.base import Model
+from nems.models.base import Model, Model_List
 from nems.tools.demo_data.file_management import download_demo, load_demo
 from nems.tools import log
