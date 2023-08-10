0f283a6b0e1443776e4a10c269420534f25abcad
najir <irrperks@gmail.com>
2023-08-10 16:02:52 -0700

    Various changes and reorganization to make sure tutorial are working and to add basic/advanced sections

--- tutorials/8_freeze_parameters.py
+++ tutorials/8_freeze_parameters.py
@@ -5,7 +5,24 @@ import matplotlib.pyplot as plt
 
 from nems import Model
 from nems.layers import WeightChannels, FIR, DoubleExponential
+# Creating fake data, see "1_simple_fit" tutorial for more information
+input = np.random.rand(1000, 18)  # 18-channel spectrogram stimulus
+target = np.stack(input[:, :5])   # PSTH response of a single neuron
 
+# This indicates that our code is interactive, allowing a
+# matplotlib backend to show graphs
+#plt.ion()
+
+# Options parameters for demonstration, see more at: scipy.optimize.minimize
+initialization = {'options': {'ftol': 1e3, 'maxiter': 10}}
+model = Model()
+model.add_layers(
+    WeightChannels(shape=(18,4), name='wc'),
+    FIR(shape=(25, 4), name='fir'),
+    DoubleExponential(shape=(1,), name='dexp'),
+)
+
+############GETTING STARTED###############
 ########################################################
 # Freezing Parameters
 # When fitting models we've found that seperating linear and non-linear steps
@@ -18,50 +35,22 @@ from nems.layers import WeightChannels, FIR, DoubleExponential
 #
 # Note: It will be helpful to keep track of your layers index and/or provide names
 ########################################################
-
-# Creating fake data, see "1_simple_fit" tutorial for more information
-input = np.random.rand(1000, 18)  # 18-channel spectrogram stimulus
-target = np.stack(input[:, :5])   # PSTH response of a single neuron
-
-# This indicates that our code is interactive, allowing a
-# matplotlib backend to show graphs
-plt.ion()
-
-###########################
-# Building a standard linear model
-#
-# - Layers have parameter name="some_name"
-#   - This will be used for freezing specific layers
-# - Double Exponential shape=(x,) creates a non-linear layer
-###########################
-model = Model()
-model.add_layers(
-    WeightChannels(shape=(18,4), name='wc'),
-    FIR(shape=(25, 4), name='fir'),
-    DoubleExponential(shape=(1,), name='dexp'),
-)
-
-# Options parameters for demonstration, see more at: scipy.optimize.minimize
-initialization = {'options': {'ftol': 1e3, 'maxiter': 10}}
-
-# Layer function will skip non-linear parameters, while still modifying linear ones
-# for example: this results in a scalar offset: `output = input + shift`).
-model.layers['dexp'].skip_nonlinearity()
+model.freeze_layers('wc', 'fir')
 
 # Show the number of frozen (untrainable), unfrozen (trainable),
 # permanent (always untrainable), and total parameter values for each layer.
-print(f'Parameters for first fit:\n{model.parameter_info}\n')
+print(f'Parameters for first fit: {model.parameter_info}\n')
 
-# Note that fit returns a copy of the model. In this case we choose to re-use
-# the same model name (i.e. the unfit model will be overwritten).
+# Fit the model again, using the previous fit as a starting point.
 model = model.fit(input=input, target=target, fitter_options=initialization)
 
-# Plot our model after the first fit
+# Plot our model after the second fit
 model.plot(input, target)
 
-# Opens our non-linear paramenters back up to modification again
-model.layers['dexp'].unskip_nonlinearity()
 
+
+
+############ADVANCED###############
 ###########################
 # Freezing groups of layers
 # 
@@ -69,14 +58,21 @@ model.layers['dexp'].unskip_nonlinearity()
 #   - Integer: We can do this my providing indexs of layers
 #   - Keyword: We can use our defined names for layers instead
 ###########################
-model.freeze_layers('wc', 'fir')
-print(f'\nParameters for second fit:\n{model.parameter_info}\n')
+# Layer function will skip non-linear parameters, while still modifying linear ones
+# for example: this results in a scalar offset: `output = input + shift`).
+model.layers['dexp'].skip_nonlinearity()
+print(f'Parameters for second fit: {model.parameter_info}\n')
 
-# Fit the model again, using the previous fit as a starting point.
+# Note that fit returns a copy of the model. In this case we choose to re-use
+# the same model name (i.e. the unfit model will be overwritten).
 model = model.fit(input=input, target=target, fitter_options=initialization)
 
-# Plot our model after the second fit
+# Plot our model after another fit
 model.plot(input, target)
+
+# Opens our non-linear paramenters back up to modification again
+model.layers['dexp'].unskip_nonlinearity()
+
 
 ###########################
 # Unfreezing Layers
