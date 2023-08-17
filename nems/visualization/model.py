f770ba5a8876234de89ff4a9fd45143ceda0dca0
Isaac Perks <irrperks@gmail.com>
2023-08-17 10:20:45 -0700

    Small position fix for legend

--- nems/visualization/model.py
+++ nems/visualization/model.py
@@ -877,7 +877,7 @@ def plot_data(data, title, label=None, target=None, ax=None,
         x_pos = ax.get_xlim()[0]
         y_pos = ax.get_ylim()[1]
         ax.text(x_pos, y_pos, title, va='top', bbox=dict(boxstyle='round, pad=.1, rounding_size=.1', alpha=.7, facecolor='white'))
-        ax.legend()
+        ax.legend(loc='upper center', bbox_to_anchor=(0.5,1.0))
     return ax
 
 # TODO: Iterate through dictionaries for larger inputs
