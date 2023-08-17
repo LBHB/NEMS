2dbdd55cc19c6cde6fb08f7b74b4c868776be3c0
Isaac Perks <irrperks@gmail.com>
2023-08-17 13:41:45 -0700

    Reorganized and cleaned up

--- tutorials/6_less_simple_fit.py
+++ tutorials/6_less_simple_fit.py
@@ -6,59 +6,16 @@ import matplotlib.pyplot as plt
 from nems import Model
 from nems.layers import WeightChannels, FIR, DoubleExponential
 from nems.layers.base import Layer, Phi, Parameter
+from nems import visualization
 
 # Fast-running toy fit options for demonstrations.
 options = {'options': {'maxiter': 2, 'ftol': 1e-2}}
 
 # This indicates that our code is interactive, allowing a
 # matplotlib backend to show graphs
-plt.ion()
+#plt.ion()
 
-########################################################
-# Creating custom layers
-# Sometimes you may need to deal with more data that our Inp, Out, and State
-# We can create our own layers built from our base layer, to include that new data
-#   initial_parameters: 
-#   evaluate: 
-# See more at: https://temp.website.net/nems_layers_base_layer
-########################################################
-
-###########################
-# Sum Layer, dummy layer that allows arbitrary number of inputs
-#   evaluate: Allows you to specify wanted inputs, and define potential operations
-#       *inputs: Any number of data, gets pushed into a flat evaluation with no operation
-# See more at: https://temp.website.net/nems_layers_base_Layer_evaluate
-###########################
-class Sum(Layer):
-    def evaluate(self, *inputs):
-        # All inputs are treated the same, no fittable parameters.
-        return np.sum(inputs)
-
-###########################
-# Linear Weighting Layer, dummy layer that allows some manipulation of provided data
-#   initial_parameters: Allows you to evoke a set of parameters that are passed to layers.parameter
-#       a: a parameter value of shape(1,)
-#       b: a second parameter value of shape(1,)
-#       output: Phi(a, b)
-#   evaluate: Same as before, In this case returns a multiplication of some set of inputs to parameters
-#       a,b: provided parameters initialized in our first function
-#       prediction, pupil, other_state: 3 various inputs
-#       output: parameter1*input1 + parameter2*input2 + input3
-# See more at: https://temp.website.net/nems_layers_base_Layer_initialparameters
-###########################
-class LinearWeighting(Layer):
-
-    def initial_parameters(self):
-        a = Parameter('a', shape=(1,))
-        b = Parameter('b', shape=(1,))
-        return Phi(a, b)
-
-    def evaluate(self, prediction, pupil, other_state):
-        # `prediction` and `pupil` are treated one way, whereas `other_state`
-        # is treated another way.
-        a, b = self.get_parameter_values('a', 'b')
-        return a*prediction + b*pupil + other_state
-
+# Creating fake data
 def my_complicated_data_loader(file_path):
     print(f'Loading data from {file_path}, but not really...')
 
@@ -77,12 +34,55 @@ def my_complicated_data_loader(file_path):
     other_state = np.random.rand(TIME, 1)
 
     return spectrogram, response, pupil_size, other_state, TIME, CHANNELS
-
 # Create variables from our data import function
 stim, resp, pupil, state, TIME, CHANNELS = my_complicated_data_loader('/path/data.csv')
 
+
+########################################################
+# Creating custom layers
+# Sometimes you may need to deal with more data that our Inp, Out, and State
+# We can create our own layers built from our base layer, to include that new data
+#   initial_parameters: 
+#   evaluate: 
+########################################################
+
 ###########################
-# A model with multiple inputs requires a Dict of keywords/values ex,
+# Sum Layer, dummy layer that allows arbitrary number of inputs
+#   evaluate: Allows you to specify wanted inputs, and define potential operations
+#       *inputs: Any number of data, gets pushed into a flat evaluation with no operation
+###########################
+class Sum(Layer):
+    def evaluate(self, *inputs):
+        # All inputs are treated the same, no fittable parameters.
+        return np.sum(inputs)
+    
+###########################
+# Linear Weighting Layer, dummy layer that allows some manipulation of provided data
+#   initial_parameters: Allows you to evoke a set of parameters that are passed to layers.parameter
+#       a: a parameter value of shape(1,)
+#       b: a second parameter value of shape(1,)
+#       output: Phi(a, b)
+#   evaluate: Same as before, In this case returns a multiplication of some set of inputs to parameters
+#       a,b: provided parameters initialized in our first function
+#       prediction, pupil, other_state: 3 various inputs
+#       output: parameter1*input1 + parameter2*input2 + input3
+###########################
+class LinearWeighting(Layer):
+
+    def initial_parameters(self):
+        a = Parameter('a', shape=(1,))
+        b = Parameter('b', shape=(1,))
+        return Phi(a, b)
+
+    def evaluate(self, prediction, pupil, other_state):
+        # `prediction` and `pupil` are treated one way, whereas `other_state`
+        # is treated another way.
+        a, b = self.get_parameter_values('a', 'b')
+        return a*prediction + b*pupil + other_state
+    
+###########################
+# A model sometimes requires multiple inputs which requires a Dict of 
+# keywords/values ex,
 #   {
 #       keyword1: input1,
 #       keyword2: input2,
@@ -91,8 +91,6 @@ stim, resp, pupil, state, TIME, CHANNELS = my_complicated_data_loader('/path/dat
 #   }
 ###########################
 input = {'stimulus': stim, 'pupil': pupil, 'state': state}
-target = resp
-
 
 ###########################
 # Layers for our model, This time in the form of a list of functions
@@ -105,7 +103,6 @@ target = resp
 #   Sum: Adds up a bunch of inputs including the LN output
 #   LinearWeighting: Sets evaluation parameters from dict keys
 #       Also keeps parameters empty, so they are infered from other layers(DOUBLE CHECK THIS)
-# See more at: https://temp.website.net/model_layers
 ###########################
 layers = [
     WeightChannels(shape=(18,4), input='stimulus'),
@@ -145,6 +142,7 @@ axes.set_title("State")
 axes = ax[2][2]
 axes.plot(range(0,TIME), (input['pupil'][:, :]*10).astype(int)) 
 axes.set_title("Pupil")
+
 ## Uncomment if you don't have an interactive backend installed
 #plt.show()
 
