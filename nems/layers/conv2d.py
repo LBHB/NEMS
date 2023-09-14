538135cd28b2fd731a2a86959dcc5030ab0261b7
Isaac Perks <irrperks@gmail.com>
2023-09-14 13:46:13 -0700

    Close to functional based on 4d Tensor inputs

--- nems/layers/conv2d.py
+++ nems/layers/conv2d.py
@@ -11,22 +11,19 @@ class Conv2d(Layer):
     NEMS compatible convolutional 2-D layer. Currently
     for tensorflow.
     '''
-    def __init__(self, filters, stride=1, pad_type='mean', pad_axes='both',
+    def __init__(self, stride=1, pad_type='zero', pad_axes='both',
                   pool_type='mean', **kwargs):
         '''
+        NOTE: Any way to make filter simpler? 
+        Filter: 4D array format [filter_height, filter_width, in_channels, out_channels]
         '''
         require_shape(self, kwargs, minimum_ndim=2)
-        self.filters     = filters
         self.stride      = stride
         self.pad_type    = pad_type
         self.pad_axes    = pad_axes
         self.pool_type   = pool_type
         super().__init__(**kwargs)
 
-        # NOTE: Confirm best course for getting kernel_size values
-        self.kernel_size = self.shape
-
-    
     def initial_parameters(self):
         '''
         '''
@@ -37,64 +34,59 @@ class Conv2d(Layer):
                                  prior=prior)
         return Phi(coefficients)
 
-    def evaluate(self):
+    def evaluate(self, input):
         '''
         '''
-        self.convolution()
-        return
+        return self.convolution(input)
 
-    def convolution(self):
+    def convolution(self, input):
         '''
         '''
-        input_tensor = tf.constant(self.input)
-        filter_tensor = tf.constant()
-        self.pad()
-        tf.nn.conv2d(input_tensor, filter_tensor, self.stride, padding='VALID')
-        self.pooling()
-        return
-    
-    def pooling(self):
-        '''
-        '''
-        if self.pool_type == 'absmax':
-            pass
-        elif self.pool_type == 'absmin':
-            pass
-        # Default mean pooling
-        else:
-            pass
-        tf.nn.pool()
-        return
-    
-    def pad(self):
-        '''
-        '''
-        if self.pad_type == 'absmax':
-            pass
-        elif self.pad_type == 'absmin':
-            pass
-        # Default mean padding
-        else:
-            pass
+        #input_tensor = tf.cast(tf.reshape(input, [1, input.shape[0], input.shape[1], 1] ), tf.float32)
+        input_tensor = input
+        filter_tensor = tf.cast(tf.reshape(self.coefficients, [self.coefficients.shape[0], self.coefficients.shape[1], 1, self.coefficients.shape[-1]]), tf.float32)
+        if self.pad_type != 'None':
+            self.pad(input_tensor)
         
-        if self.pad_axes == 'x':
-            pass
-        elif self.pad_axes == 'y':
-            pass
-        # Default both axes
-        else:
-            pass
+        convolved = []
+        for idx in self.coefficients[-2]:
+            input_convolution = tf.nn.conv2d(input_tensor, filter_tensor[...,idx], self.stride, padding='VALID')
+            convolved.append(input_convolution)
 
-        tf.pad()
-        return
+        return self.pooling(convolved)
     
-    def weights_to_phi(self):
+    def pooling(self, input_tensor):
         '''
-        Returns Phi built via weights of tensor
         '''
-        phi = {'kernel': self.kernel.numpy(),
-               'bias': self.bias.numpy()}
-        return phi
+        return tf.nn.pool(input_tensor, self.coefficients[0:1], pooling_type=self.pool_type)
+    
+    def pad(self, input_tensor):
+        '''
+        NOTE: Implement custom sizing for padding? 
+        '''
+        y_axes, x_axes = (1, 1)
+        if self.pad_axes == 'x':
+            y_axes = 0
+        elif self.pad_axes == 'y':
+            x_axes = 0
+
+        y_kernel = int(self.coefficients.shape[1]/2+1)*y_axes
+        x_kernel = int(self.coefficients.shape[0]/2+1)*x_axes
+        pad_amounts = [y_kernel, x_kernel]
+        pad_array = tf.constant([[pad_amounts[0],pad_amounts[0] ], [pad_amounts[1], pad_amounts[1]]])
+
+        if self.pad_type == 'reflect':
+            input_tensor = tf.pad(input_tensor, pad_array, mode='REFLECT')
+        elif self.pad_type == 'symmetric':
+            input_tensor = tf.pad(input_tensor, pad_array, mode='SYMMETRIC')
+        else:
+            pad_constant = 0
+            if self.pad_type == 'max':
+                pad_constant = np.max(np.abs(self.input))
+            elif self.pad_type == 'min':
+                pad_constant = np.min(np.abs(self.input))
+            input_tensor = tf.pad(input_tensor, pad_array, mode='CONSTANT', constant_values=pad_constant)
+        return input_tensor
 
     @layer('c2d')
     def from_keyword(keyword):
