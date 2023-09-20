930f424c8eb8e6e3de0292d14848d07cbc629c40
Najir <irrperks@gmail.com>
2023-09-20 15:57:44 -0700

    Cleaned up previous code and rearranged output format to match fir pattern. Further tested input/outputs. Added basic keywords functionality, although untested

--- nems/layers/conv2d.py
+++ nems/layers/conv2d.py
@@ -7,38 +7,84 @@ from nems.registry import layer
 from nems.tools.arrays import broadcast_axes
 
 
-# NOTE: WIP, currently only partially-functional
+# NOTE: WIP, some things may not work as intended
 class Conv2d(Layer):
     '''
     NEMS compatible convolutional 2-D layer. Currently
     for tensorflow.
 
-    Shape should be of format;
-    (Batch x Time x Neurons x Filters) or,
-    (Time x Neurons x Filters) or,
+    Parameters
+    ----------
+    shape: N-Tuple
+        Determines the shape of the filter used on the
+        given input through Conv2D. First 2 dimensions
+        determine filter size, last dimension is used
+        for amount of filters, batch dimension used in
+        conv2d function
+
+        Shape should be of format;
+        (Time x Neurons x Batch x Filters) or,
+        (Time x Neurons x Filters) or,
+        (Time x Neurons)
+
+    See also
+    --------
+    nems.layers.base.Layer
+
+    Other Info
+    ----------
+    If batches are needed in filters, full 4-D shape must be provided.
+
+    If shape == 3, we assume filters are provided, and include defaults for batches
+    If shape == 2, we provided defaults for filters and batches
+
+    Input format is;
+    (Batch x Time x Neurons x Out_Channels) or,
+    (Batch x Time x Neurons) or,
     (Time x Neurons)
-
-    If batches are needed, full 4-D shape must be provided.
-
-    if shape == 3, we assume filters are provided, and include defaults for batches
-    if shape == 2, we provided defaults for filters and batches
 
     '''
     def __init__(self, stride=1, pad_type='zero', pad_axes='both',
-                  pool_type='mean', **kwargs):
+                  pool_type='AVG', **kwargs):
         '''
+        Initializes layer with given parameters
+
+        Parameters
+        ----------
+        stride: Tuple-Int
+            Provides stride values to be used in convolutions
+        pad_type: String
+            Type of padding to be used on input data. See
+            self.pad for more info
+        pad_axes: String
+            Axes to apply padding to our input
+        pool_type: String
+            Type of pooling to be used on filters dimension reduction.
+            See self.pool for more info 
         '''
         require_shape(self, kwargs, minimum_ndim=2)
         self.stride      = stride
         self.pad_type    = pad_type
         self.pad_axes    = pad_axes
         self.pool_type   = pool_type
+        self.pad_indices = [0, 0, 0, 0]
         super().__init__(**kwargs)
 
     def initial_parameters(self):
         '''
         Saves important paramenters such as input data shape, to be used
         in evaluation.
+
+        Layer parameters
+        ----------------
+        coefficients : ndarray
+            Shape matches `FIR.shape`.
+            Prior:  Normal(mean=0, sd=1/size)
+            Bounds: (-np.inf, np.inf)
+
+        Returns
+        -------
+        nems.layers.base.Phi
         '''
         mean = np.full(shape=self.shape, fill_value=0.0)
         sd = np.full(shape=self.shape, fill_value=1/np.prod(self.shape))
@@ -46,74 +92,48 @@ class Conv2d(Layer):
         coefficients = Parameter(name='coefficients', shape=self.shape,
                                  prior=prior)
         return Phi(coefficients)
-    
-    @property
-    def coefficients(self):
-        """Filter that will be convolved with input.
-        
-        Re-parameterized subclasses should overwrite this so that `evaluate`
-        doesn't need to change.
-
-        Returns
-        -------
-        coefficients : ndarray
-            coefficients.shape = WeightChannels.shape
-        
-        """
-        return self.parameters['coefficients'].values
 
     def evaluate(self, input):
         '''
         '''
         filter_tensor = self.shape_filter(self.coefficients)
         input_tensor, filter_tensor = self.shape_tensor(input, filter_tensor)
-        #input_tensor = tf.cast(tf.reshape(input, shape=[1, input.shape[0], input.shape[1], 1]), tf.float32)
-        #filter_tensor = tf.cast(tf.Variable(tf.constant(0, shape=[self.coefficients.shape[0], self.coefficients.shape[1], 1, 1])), tf.float32)
         if self.pad_type != 'None':
             input_tensor = self.pad(input_tensor)
             
-        # Should return data in original format of Batch x Time x Neurons
-        return self.convolution(input_tensor, filter_tensor)
+        # Should return data in format of Time x Neurons with padding removed
+        pad_indices = self.pad_indices
+        convolved_tensor = self.convolution(input_tensor, filter_tensor)[0, pad_indices[0]:pad_indices[1], pad_indices[2]:pad_indices[3]]
+        return convolved_tensor
 
-    def shape_filter(self, coefficents):
+    def shape_filter(self, coefficients):
         '''
-        Adds filter dims if non are provided.
+        Checking existing dimensions and adding new ones if needed
+        to create 4D array filter. 
 
-        NOTE: Is flipping rank needed in conv2d?
-        Flips non time/filter dimensions for convolutions
+        if ndim == 2, Add empty batch, out dimensions
+        if ndim == 3, Add empty batch dimension
+
+        When shaping our tensor, last dimension of filter/input
+        are broadcasted
         '''
-        coefficients = self.coefficients
         if coefficients.ndim == 2:
-            # Add a dummy filter/output axis
             coefficients = coefficients[..., np.newaxis]
             coefficients = coefficients [..., np.newaxis]
         elif coefficients.ndim == 3:
             coefficients = coefficients[0:1, np.newaxis, :]
 
-
-        '''
-        NOTE: Prob delete this
-        # Coefficients are applied "backwards" (convolution) relative to how
-        # they are specified (filter), so have to flip all dimensions except
-        # time and number of filters/outputs.
-        flipped_axes = [1]  # Always flip rank
-        other_dims = coefficients.shape[2:-1]
-        for i, d in enumerate(other_dims):
-            # Also flip any additional dimensions
-            flipped_axes.append(i+2)
-        coefficients = np.flip(coefficients, axis=flipped_axes)
-        '''
-
-
         return coefficients
     
     def shape_tensor(self, input_tensor, coefficients):
         '''
-        Shapes our input data to fit conv2d if non-4D is provided
-        
+        Shapes our input data to fit conv2d if non-4D is provided.
+        Last dimension of tensor/filter are also broadcasted.
+
+        if ndim == 2, Add empty batch, and filter dimension
+        if ndim == 3, Add empty filter dimension
         '''
         if input_tensor.ndim == 2:
-            # Add a dummy filter/output axis
             input_tensor = input_tensor[..., np.newaxis]
             input_tensor = input_tensor[np.newaxis, ...]
         elif input_tensor.ndim == 3:
@@ -140,23 +160,35 @@ class Conv2d(Layer):
 
     def convolution(self, input_tensor, filter_tensor):
         '''
-        Convolutions on input_tensor with given filter_tensor.
-        Total convolutions are stacked, and reshaped to the same
-        dimension as original input, before being pooled.
+        Convolutions on input_tensor with given filter_tensor and
+        stride. Completed convolutions are sent back to pool to reduce
+        dimensions
         '''
-        #input_convolutions = [tf.nn.conv2d(input_tensor, filter_tensor, self.stride, padding='VALID') 
-        #                     for idx in range(filter_tensor.shape[-1])]
         input_convolutions = tf.nn.conv2d(input_tensor, filter_tensor, self.stride, padding='VALID') 
-        #convolved = tf.stack(input_convolutions, -1)
-        #reshape_height = int(convolved.shape[0]*convolved.shape[1]/2)
-        #reshape_width = int(convolved.shape[0]*convolved.shape[2]/2)
-        #convolved = tf.reshape(convolved, [1, reshape_height, reshape_width, 1])
 
         return self.pool(input_convolutions)
     
     def pool(self, input_tensor):
         '''
-        temp tests
+        Pooling of total filters created from conv2d function. Done via
+        dimension reduction on the filter dimension via given pool_type.
+
+        Default reduction is MEAN
+
+        NOTE: Other reductions may be added/removed
+
+        Parameters
+        ----------
+        input_tensor: 4D Tensor
+            Given tensor with completed convolutions to pool
+        pool_type: String
+            String value to determine type of reduction used. Includes:
+            AVG - Reduction via average values along tensor 
+            MIN - Reduction via minimum values 
+            PROD - Reduction via production of values
+            STD - Reduction via standard deviation
+            SUM - Reduction via sum values
+
         '''
         pool_type = self.pool_type
         if pool_type == 'AVG':
@@ -178,11 +210,31 @@ class Conv2d(Layer):
     def pad(self, input_tensor):
         '''
         Pads X and Y dimensions of input, for use with convolutions and strides
+        Uses total shape/size of filter window to determine amount of padding. 
+        Currently no way to provide a specified size.
 
         NOTE: Implement custom sizing for padding? 
+
+        Parameters
+        ----------
+        input_tensor: 4D-Tensor
+            Input tensor that we wish to pad values with
+        pad_type: String
+            String value to determine type of padding used. Includes:
+            reflect - pads with reflection of edge values
+            symmetric - 
+            max - pads with abs max values
+            min - pads with abs min values
+            zero - pads with 0 values
+
+            Default is constant of 0's
         '''
         y_pad = int(self.coefficients.shape[1]/4+1)
         x_pad = int(self.coefficients.shape[0]/4+1)
+
+        # Saving pad indices to remove later
+        self.pad_indices = [int(x_pad/2)-1, int(input_tensor.shape[1]-(x_pad/2))+1, int(y_pad/2)-1, int(input_tensor.shape[2]-(y_pad/2))+1]
+
         if self.pad_axes == 'x':
             y_pad = 0
         elif self.pad_axes == 'y':
@@ -203,14 +255,6 @@ class Conv2d(Layer):
             input_tensor = tf.pad(input_tensor, pad_array, mode='CONSTANT', constant_values=pad_constant)
         return input_tensor
 
-    @layer('c2d')
-    def from_keyword(keyword):
-        '''
-        Keyword layer calls. 'c2d' should call this function
-        with default values
-        '''
-        return
-    
     @property
     def coefficients(self):
         """Filter that will be convolved with input.
@@ -225,3 +269,31 @@ class Conv2d(Layer):
         
         """
         return self.parameters['coefficients'].values
+
+    @layer('conv2d')
+    def from_keyword(keyword):
+        '''
+        Keyword layer call. 'conv2d' should call this layer
+        with default values
+        
+                Keyword options
+        ---------------
+        {digit}x{digit}x ... x{digit} : N-dimensional shape.
+            (time, input channels a.k.a. rank, ..., output channels) 
+
+        See also
+        --------
+        Layer.from_keyword
+        
+        '''
+        kwargs = {}
+        Conv2d_class = Conv2d
+
+        options = keyword.split('.')
+        kwargs['shape'] = pop_shape(options)
+        for op in options:
+            if op.startswith('s'):
+                kwargs['stride'] = int(op[1:])
+        conv = Conv2d_class(**kwargs)
+
+        return conv
