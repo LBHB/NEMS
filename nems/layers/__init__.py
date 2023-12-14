88c6c1b344de8c40e8d0c901c22a76083ce91b4c
Stephen David <stephen.v.david@gmail.com>
2023-12-13 17:56:52 -0800

    cleaning up projection, testing HRTF layer

--- nems/layers/__init__.py
+++ nems/layers/__init__.py
@@ -9,7 +9,7 @@ from .nonlinearity import LevelShift, DoubleExponential, RectifiedLinear, ReLU, 
 from .filter import FiniteImpulseResponse, FIR, STRF
 from .weight_channels import WeightChannels, WeightChannelsMulti, WeightChannelsGaussian, \
     WeightChannelsMultiGaussian, WeightGaussianExpand
-from .state import StateGain, StateDexp, StateHinge
+from .state import StateGain, StateDexp, StateHinge, HRTF
 from .numpy import NumPy
 from .stp import ShortTermPlasticity, STP
 from .algebra import SwapDims, ConcatSignals, MultiplySignals, MultiplyByExp
